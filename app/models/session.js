f057ab06f0032c5518fc3cac07f8157665583d96
Manda Wilson <wilson@cbio.mskcc.org>
2016-02-09 22:56:28 +0000

    changed from meanjs to springboot

--- app/models/session.js
+++
@@ -1,8 +0,0 @@
-// app/models/session.js
-
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
-
-var SessionSchema   = new Schema({data: Schema.Types.Mixed});
-
-module.exports = mongoose.model('Session', SessionSchema);
